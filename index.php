37d6015d20a248b0e80644c3ac487ae0f4a7a764
teacarolina <teacarolina@icloud.com>
2021-02-26 11:58:18 +0100

    login function

--- index.php
+++ index.php
@@ -4,19 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Home Page</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<div class="login-page">
-  <div class="form">
-    <form class="login-form" action="" method="">
-      <input type="text" placeholder="username"/>
-      <input type="password" placeholder="password"/>
-      <input id="button" type="submit" value="login">
-      <p class="message">Not registered? <a href="register.php">Create an account</a></p>
-    </form>
-  </div>
-</div>
+    
 </body>
 </html>
