b307ed88c9d8f27022ec6c20423d6f4567bc093e
emiliajosefsson <emilia.josefssons@gmail.com>
2021-03-03 16:52:06 +0100

    removed test file

--- includes/database_connection.php
+++ includes/database_connection.php
@@ -9,9 +9,3 @@ $pdo = new PDO($dsn, $user, $password);
 
 
 
-
-
-<!-- SELECT Comment, Username, CommentDate, Title, Entry, EntryDate, CategoryId, Entries.Id 
-FROM Comments 
-JOIN Users ON Comments.UsersId = Users.Id 
-JOIN Entries ON Comments.EntriesId = Entries.Id WHERE Entries.Id = $entryId AND EntriesId = $entryId; -->
